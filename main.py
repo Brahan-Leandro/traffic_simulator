7aeb6e005e247a7be0f4576eefb1d7dce36d6c39
rosemberg <rosembergomez123@gmail.com>
2024-07-25 13:41:24 -0500

    Version Final con algunos comentarios pero sin limpiar codigo

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
+"""
+Este archivo contiene la funcion principal donde se empieza a correr el programa
+Dentro de la funcion main hay otra funcion llamada update, aquella funcion es 
+la que actualizará el estado general de la simulacion, es decir la funcion update
+es la que estara llamando cada instante para calcular el estado posterior de la simulacion.
+Esta funcion NO esta dentro de un ciclo ya que se aprovecha que la libreria de matplotlib
+(utilizada para plotear la grafica) llama a esta funcion update en cada instante, lo anterior 
+está en la linea que contiene an.FuncAnimation, se logra ver que el segundo argumento de la 
+funcion FuncAnimation es la funcion update, en general ese es funcionamiento  en los siguientes 
+comentarios se especifica cada variable 
+"""
+# Importando librerias para plotear y generar los numeros aleatorios
 from random import random 
 import matplotlib.pyplot as plt 
 import matplotlib.animation as an
 import matplotlib as mpl
 from matplotlib.colors import ListedColormap
 import numpy as np
-
-from carro import Carro
-from vacio import Vacio
-from semaforos import Sistema_Semaforos
-from parametros import *
-from generador_carros import generar_carro						   
+# Importando las Clases que se utilizan para la simulacion
+from carro import Carro # clase  especifica un carro  que tiene  velocidad, direccion,posicion... etc
+from vacio import Vacio # Clase especifica el espacio, el cual contiene 
+#los carros y ademas se encarga de calcular  la logica del estado posterior mediante la funcion calcular_avance
+from semaforos import Sistema_Semaforos # Clase que controla el estado de los semaforos
+from parametros import *   # Contiene todos los parametros que especifican las caracteristicas de la simulacion 
+from generador_carros import generar_carro # Se es una duncion que encarga de intanciar nuevos carros						   
 def main():
 	cmap, norm = cmap_norm()
-	T=[]
-	semaforos = Sistema_Semaforos(tiempo_verde,tiempo_amarillo,modo_semaforos)
-	espacio = Vacio(tamano_mapa,rapidez_max,probabilidad_frenar,semaforos.estado)
-	densidad_salida_Norte =[]
-	def update(i):
-		semaforos.actualizar_estado(i)
-		espacio.actualizar_semaforo(semaforos.estado)
-		espacio.condiciones_carros()
+	T=[]     # Lista que contiene el numeros que indican el fotograma ejemplo [1,2,3,4] en este caso la lista tiene 4 fotogramas
+	semaforos = Sistema_Semaforos(tiempo_verde,tiempo_amarillo,modo_semaforos) # Instancia para los semaforos
+	espacio = Vacio(tamano_mapa,rapidez_max,probabilidad_frenar,semaforos.estado) # Instancia para los Espacio
+	# Se emplea  para graficar la densidad de de carros  por cada tramo
+	densidad_entrada_Norte =[] 
+	densidad_entrada_Sur =[]
+	densidad_entrada_Este =[]
+	densidad_entrada_Oeste =[]
+	# Se emplea  para graficar la intensidad de de carros  por cada tramo
+	intensidad_norte=[]
+	intensidad_sur=[]
+	intensidad_este=[]
+	intensidad_oeste=[]
+	
+	def update(i): 
+		# Esta funcion se es la que se encarga de controlar el ciclo general del programa
+		# se llama en cada instante y las operaciones que hace son en el siguiente orden
+		
+		semaforos.actualizar_estado(i) # Actualizar el estado de los semaforos es decir en cada instante el objeto semaforos 
+									   # va a cambiar segun vaya avanzando la simulacion 
+		espacio.actualizar_semaforo(semaforos.estado) # Se le da informacion al manejador general (Espacio) del estado del semaforo
+		espacio.condiciones_carros()  # Se revisa las condiciones de frenado
+		
+		# las siguientes condiciones que determinan si se debe agregar un nuevo carro a la salida Norte, Sur, Oeste, Este respectivamente 
+		# al considerar la probabilidad N_densidad,S_densidad, W_densidad,E_densidad
 		if random()<N_densidad:
 			espacio.agregar_carro(generar_carro("Norte"))
 		if random()<S_densidad:
@@ -28,8 +58,18 @@ def main():
 			espacio.agregar_carro(generar_carro("Oeste"))
 		if random()<E_densidad:
 			espacio.agregar_carro(generar_carro("Este"))
-		matris_plot = espacio.calcular_avance()	
-		densidad_salida_Norte.append(calcular_densidad(matris_plot,"entrada_norte"))
+			
+		matris_plot,i_norte,i_sur,i_este,i_oeste = espacio.calcular_avance() # Se calcula el 
+		
+		intensidad_norte.append(i_norte)
+		intensidad_sur.append(i_sur)
+		intensidad_este.append(i_este)
+		intensidad_oeste.append(i_oeste)
+			
+		densidad_entrada_Norte.append(calcular_densidad(matris_plot,"entrada_norte"))
+		densidad_entrada_Sur.append(calcular_densidad(matris_plot,"entrada_sur"))
+		densidad_entrada_Este.append(calcular_densidad(matris_plot,"entrada_este"))
+		densidad_entrada_Oeste.append(calcular_densidad(matris_plot,"entrada_oeste"))
 		plt.clf()
 		#print(i)
 		T.append(i)
@@ -38,18 +78,52 @@ def main():
 	fig = plt.figure(200)
 	ani = an.FuncAnimation(fig,update,frames=pasos,interval=rapidez_simulacion,repeat=False)
 	plt.show()
-	fig2 = plt.figure(300)
-	#T = [t for t in range(pasos+1)]
-	plt.plot(T,densidad_salida_Norte)
+	fig2 = plt.figure("Densidad Norte Sur Dos Semaforos")
+	plt.plot(T,densidad_entrada_Norte,label="Entrada norte")
+	plt.plot(T,densidad_entrada_Sur,label="Entrada sur")
+	plt.legend(loc='upper right')
+	
+	fig3 = plt.figure("Densidad Este oeste Dos Semaforos")
+	plt.plot(T,densidad_entrada_Este,label="Entrada Este")
+	plt.plot(T,densidad_entrada_Oeste,label="Entrada Oeste")
+	plt.legend(loc='upper right')
+		
+	fig4 = plt.figure("Intensidad Norte Sur")
+	plt.plot(T,intensidad_norte,label="Entrada Norte")
+	plt.plot(T,intensidad_sur,label="Entrada Sur")
+	plt.legend(loc='upper right')
+	
+	fig4 = plt.figure("Intensidad Este Oeste")
+	plt.plot(T,intensidad_este,label="Entrada Este")
+	plt.plot(T,intensidad_oeste,label="Entrada Oeste")
+	plt.legend(loc='upper right')
 	plt.show()
 
 def calcular_densidad(matris,via):
 	numero_carros = 0
+	tamano = 0
 	if via == "entrada_norte":
 		for y in range(int(tamano_mapa["y"]/2)-1):
-			if matris[int(tamano_mapa["x"]/2)][y]==1:
+			tamano+=1
+			if matris[int(tamano_mapa["x"]/2)][y]==1 or matris[int(tamano_mapa["x"]/2)][y]==8:
 				numero_carros +=1
-	return numero_carros/(tamano_mapa["y"]/2-1)
+				#return numero_carros/(tamano_mapa["y"]/2-1)
+	if via == "entrada_sur":
+		for y in range(int(tamano_mapa["y"]/2)+2,tamano_mapa["y"]):
+			tamano+=1
+			if matris[int(tamano_mapa["x"]/2)+1][y]==1 or matris[int(tamano_mapa["x"]/2)+1][y]==8:
+				numero_carros +=1
+	if via == "entrada_este":
+		for x in range(int(tamano_mapa["x"]/2)+2,tamano_mapa["x"]):
+			tamano+=1
+			if matris[x][int(tamano_mapa["y"]/2)]==1 or  matris[x][int(tamano_mapa["y"]/2)]==8:
+				numero_carros +=1
+	if via == "entrada_oeste":
+		for x in range(int(tamano_mapa["x"]/2)-1):
+			tamano+=1
+			if matris[x][int(tamano_mapa["y"]/2+1)]==1 or matris[x][int(tamano_mapa["y"]/2+1)]==8:
+				numero_carros +=1
+	return numero_carros/(tamano)
 def cmap_norm():
 	levels = [0,1,7,8, 10, 20, 30,40]
 	colors = [[128/255,128/255,128/255], #0-1   Color Vias
--- vacio.py
+++ vacio.py
@@ -5,7 +5,7 @@ class Vacio:
 	def __init__(self,tamano_mapa,rapidez_max,probabilidad_frenar,estado_semaforos):
 		self.rapidez_max=rapidez_max
 		self.carros = []
-		self.celdas = [[0 for y in range(tamano_mapa["y"])] for x in range(tamano_mapa["x"])]
+		#self.celdas = [[0 for y in range(tamano_mapa["y"])] for x in range(tamano_mapa["x"])]
 		self.carros_A = [[0 for y in range(tamano_mapa["y"])] for x in range(tamano_mapa["x"])]
 		self.tamano_mapa = tamano_mapa
 		self.probabilidad_frenar=probabilidad_frenar
@@ -25,26 +25,39 @@ class Vacio:
 			return True
 		else:
 			return False
-	def agregar_celda(self, celda):
-		self.celdas[celda.posicion[0]][celda.posicion[1]]=celda
+	#def agregar_celda(self, celda):
+	#	self.celdas[celda.posicion[0]][celda.posicion[1]]=celda
 	def calcular_avance(self):
+		intensidad_norte = 0
+		intensidad_sur = 0
+		intensidad_este = 0
+		intensidad_oeste = 0
 		for carro in self.carros:
 			v_x = carro.velocidad[0]
 			v_y = carro.velocidad[1]
 			a_borrar = []
 			carro_eliminado = False
 			for m in range(0,carro.rapidez):
+				# ~ print(self.carros_A[int(self.tamano_mapa["x"]/2+1)][int(self.tamano_mapa["y"]/2)])
+				if self.condicion_anti_atascos(carro):
+					carro.detenerse()
+					break
 				if (self.validar_futura_frontera(carro)):
 					if self.carros_A[carro.proxima_posicion()[0]][carro.proxima_posicion()[1]] == 0:
 						self.carros_A[carro.proxima_posicion()[0]][carro.proxima_posicion()[1]]=1
 						self.carros_A[carro.posicion[0]][carro.posicion[1]]=0
 						if carro.tamano > 1:
+							self.carros_A[carro.proxima_posicion()[0]][carro.proxima_posicion()[1]]=8
 							a_borrar.append([carro.posicion[0]-carro.direccion[0],carro.posicion[1]-carro.direccion[1]])
 							if carro.posicion == carro.celda_decision:
 								a_borrar.append([carro.posicion[0]-carro.salida[0],carro.posicion[1]-carro.salida[1]])
+						intensidad_norte += self.contar_intensidad(carro,"Norte")
+						intensidad_sur += self.contar_intensidad(carro,"Sur")
+						intensidad_este += self.contar_intensidad(carro,"Este")
+						intensidad_oeste += self.contar_intensidad(carro,"Oeste")
+						carro.avanzar()				
 						
-						carro.avanzar()
-						
+		
 						self.condiciones_semaforos(carro)
 						if carro.rapidez == 0:
 							break 
@@ -69,7 +82,8 @@ class Vacio:
 			except:
 				pass
 				
-		return self.carros_A
+		return self.carros_A,intensidad_norte,intensidad_sur,intensidad_este,intensidad_oeste
+
 	def eliminar_carro(self,carro):
 		for k in range(carro.tamano+2):
 			self.carros_A[carro.posicion[0]-k*carro.direccion[0]][carro.posicion[1]-k*carro.direccion[1]]=0
@@ -80,6 +94,9 @@ class Vacio:
 		mayor = carro.proxima_posicion()[0] > 0 and carro.proxima_posicion()[1] > 0
 		return menor and mayor
 	def condiciones_carros(self):
+		# Este metodo se encarga de controlar las condiciones de frenado de los carros 
+		# Basicamente recorre la lista de carros y si la velocidad es menor que la rapidez maxima esta aumenta la velocidad en 1
+		# Ademas, en la segunda condicion se evalua la probabilidad de reducir la velocidad en 1 
 		for carro in self.carros:
 			if carro.rapidez<self.rapidez_max:
 				velocidad_m = [carro.velocidad[0]+carro.direccion[0],carro.velocidad[1]+carro.direccion[1]]
@@ -99,42 +116,92 @@ class Vacio:
 		if estado_semaforos[0] == 0:
 			self.carros_A[mx2-1-1][my2-2-1:my2-1] = [10,10]
 			self.carros_A[mx2-2-1][my2-2-1:my2-1] = [10,10]
-		elif estado_semaforos[0] != 0:
+		elif estado_semaforos[0] == 2:
+			self.carros_A[mx2-1-1][my2-2-1:my2-1] = [41,41]
+			self.carros_A[mx2-2-1][my2-2-1:my2-1] = [41,41]
+		else:
 			self.carros_A[mx2-1-1][my2-2-1:my2-1] = [20,20]
 			self.carros_A[mx2-2-1][my2-2-1:my2-1] = [20,20]
+			
 		if estado_semaforos[1] == 0:
 			self.carros_A[mx2-1-1][my2+2+1:my2+4+1] = [10,10]
 			self.carros_A[mx2-2-1][my2+2+1:my2+4+1] = [10,10]
-		elif estado_semaforos[1] != 0:
+		elif estado_semaforos[1] == 2:
+			self.carros_A[mx2-1-1][my2+2+1:my2+4+1] = [41,41]
+			self.carros_A[mx2-2-1][my2+2+1:my2+4+1] = [41,41]
+		else:
 			self.carros_A[mx2-1-1][my2+2+1:my2+4+1] = [20,20]
 			self.carros_A[mx2-2-1][my2+2+1:my2+4+1] = [20,20]
 		if estado_semaforos[2] == 0:
 			self.carros_A[mx2+2+1][my2+2+1:my2+4+1] = [10,10]
 			self.carros_A[mx2+2+1+1][my2+2+1:my2+4+1] = [10,10]
-		elif estado_semaforos[2] != 0:
+		elif estado_semaforos[2] ==2:
+			self.carros_A[mx2+2+1][my2+2+1:my2+4+1] = [40,40]
+			self.carros_A[mx2+2+1+1][my2+2+1:my2+4+1] = [40,40]
+		else:
 			self.carros_A[mx2+2+1][my2+2+1:my2+4+1] = [20,20]
 			self.carros_A[mx2+2+1+1][my2+2+1:my2+4+1] = [20,20]
 		if estado_semaforos[3] == 0:
 			self.carros_A[mx2+2+1][my2-2-1:my2-1] = [10,10]
 			self.carros_A[mx2+2+1+1][my2-2-1:my2-1] = [10,10]
-		elif estado_semaforos[3] != 0:
+		elif estado_semaforos[3] == 2:
+			self.carros_A[mx2+2+1][my2-2-1:my2-1] = [40,40]
+			self.carros_A[mx2+2+1+1][my2-2-1:my2-1] = [40,40]
+		else:
 			self.carros_A[mx2+2+1][my2-2-1:my2-1] = [20,20]
 			self.carros_A[mx2+2+1+1][my2-2-1:my2-1] = [20,20]
 
-		print(estado_semaforos)
+		# ~ print(estado_semaforos)
 	def condiciones_semaforos(self,carro):
 
-		if carro.posicion[0]==int(self.tamano_mapa["x"]/2) and carro.posicion[1]== int(self.tamano_mapa["y"]/2)-2 and (self.estado_semaforos[0]==0 or self.estado_semaforos[0]==0):
+		if carro.posicion[0]==int(self.tamano_mapa["x"]/2) and carro.posicion[1]== int(self.tamano_mapa["y"]/2)-2 and (self.estado_semaforos[0]==0 or self.estado_semaforos[0]==2 ): # Condicion Carros que entran por la parte Norte
 			if carro.destino != [-1,0]:	
 				carro.detenerse()
-		if carro.posicion[0]==int(self.tamano_mapa["x"]/2)-2 and carro.posicion[1]== int(self.tamano_mapa["y"]/2)+1 and (self.estado_semaforos[1]==0 or self.estado_semaforos[1]==0):
+		if carro.posicion[0]==int(self.tamano_mapa["x"]/2)-2 and carro.posicion[1]== int(self.tamano_mapa["y"]/2)+1 and (self.estado_semaforos[1]==0 or self.estado_semaforos[1]==2 ): # Condicion Carros que entran por la parte Oeste 
 			if carro.destino != [0,1]:
 				carro.detenerse()
-		if carro.posicion[0]==int(self.tamano_mapa["x"]/2)+1 and carro.posicion[1]== int(self.tamano_mapa["y"]/2)+3 and (self.estado_semaforos[2]==0 or self.estado_semaforos[2]==0):
+		if carro.posicion[0]==int(self.tamano_mapa["x"]/2)+1 and carro.posicion[1]== int(self.tamano_mapa["y"]/2)+3 and (self.estado_semaforos[2]==0 or self.estado_semaforos[2]==2  ): # Condicion Carros que entran por la parte Sur 
 			if carro.destino != [1,0]:
 				carro.detenerse()
-		if carro.posicion[0]==int(self.tamano_mapa["x"]/2)+3 and carro.posicion[1]== int(self.tamano_mapa["y"]/2) and (self.estado_semaforos[3]==0 or self.estado_semaforos[3]==0):
+		if carro.posicion[0]==int(self.tamano_mapa["x"]/2)+3 and carro.posicion[1]== int(self.tamano_mapa["y"]/2) and (self.estado_semaforos[3]==0 or self.estado_semaforos[3]==2 ): # Condicion Carros que entran por la parte Este
 			if carro.destino != [0,-1]:
 				carro.detenerse()
+	def condicion_anti_atascos(self,carro):
+		x_2 = int(self.tamano_mapa["x"]/2)
+		y_2 = int(self.tamano_mapa["y"]/2)
+		## Condicion para carros que salen desde el norte
+		if carro.posicion == [x_2,y_2-1] and carro.destino==[1,0] and (self.carros_A[x_2+1][y_2] != 0 or self.carros_A[x_2+1][y_2+1]!=0):  
+			for carro_cruce in self.carros:
+				if carro_cruce.posicion==[x_2+1,y_2] or carro_cruce.posicion == [x_2+1,y_2+1] and carro_cruce.destino==[-1,0]:
+					return True
+		## Condicion para carros que salen desde el sur
+		elif carro.posicion == [x_2+1,y_2+2] and carro.destino==[-1,0] and (self.carros_A[x_2][y_2+1] != 0 or self.carros_A[x_2][y_2] !=0):  
+			for carro_cruce in self.carros:
+				if carro_cruce.posicion==[x_2,y_2] or carro_cruce.posicion == [x_2,y_2+1] and carro_cruce.destino==[1,0]:
+					return True
+		## Condicion para carros que salen desde el este
+		elif carro.posicion == [x_2+2,y_2] and carro.destino==[0,1] and (self.carros_A[x_2+1][y_2+1] != 0 or self.carros_A[x_2][y_2+1]!=0):  
+			for carro_cruce in self.carros:
+				if carro_cruce.posicion==[x_2+1,y_2+1] or carro_cruce.posicion == [x_2,y_2+1] and carro_cruce.destino==[0,-1]:
+					return True
+		## Condicion para carros que salen desde el oeste			
+		elif carro.posicion == [x_2-1,y_2+1] and carro.destino==[0,-1] and (self.carros_A[x_2][y_2] != 0 or self.carros_A[x_2+1][y_2]!=0):  
+			for carro_cruce in self.carros:
+				if carro_cruce.posicion==[x_2,y_2] or carro_cruce.posicion == [x_2+1,y_2] and carro_cruce.destino==[0,1]:
+					return True
 		
-			 
+		else:
+			return False	
+	def contar_intensidad(self,carro,proviene):
+		x_2 = int(self.tamano_mapa["x"]/2)
+		y_2 = int(self.tamano_mapa["y"]/2)
+		if proviene == "Norte" and  carro.posicion == [x_2,y_2-3]:
+			return 1
+		elif proviene == "Sur" and carro.posicion == [x_2+1,y_2+4]:
+			return 1
+		elif  proviene == "Este" and carro.posicion == [x_2+4,y_2]:
+			return 1
+		elif  proviene == "Oeste" and carro.posicion == [x_2-3,y_2+1]:
+			return 1
+			
+		return 0
